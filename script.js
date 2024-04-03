7955f3f2e246b90e036e801b50ad4bcb6fd12f50
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-03 05:47:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let timer = document.getElementById("timer");
+
+let currentDate = new Date();
+
+setInterval(funcion (){
+	currentDate = new Date();
+	timer.innerHTML = currentDate.toLocaleStrint();
+},1000)
